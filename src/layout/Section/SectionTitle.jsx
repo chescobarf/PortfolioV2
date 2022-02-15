d32b2254bce5ec03d9736e6c4315ad845267b35a
Christian Escobar F <christian17zxe@gmail.com>
2022-02-14 21:21:28 -0300

    Fix  Section Title

--- src/layout/Section/SectionTitle.jsx
+++ src/layout/Section/SectionTitle.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const SectionTitle = ({ title }) => {
-  return <h2 className="mb-5 uppercase text-5xl">{title}</h2>;
+  return <h2 className="mb-10 uppercase text-6xl text-gray-700">{title}</h2>;
 };
 
 export default SectionTitle;
