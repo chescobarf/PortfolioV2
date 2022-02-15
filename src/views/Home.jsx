2a1698b65db8167b9c7120c8301b9f0a8e06a2f4
Christian Escobar F <christian17zxe@gmail.com>
2022-02-14 21:22:02 -0300

    Add Home View & CSS

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -3,12 +3,24 @@ import Navbar from "../layout/Navbar";
 import Container from "../layout/Container";
 import { ProjectsList } from "../components/Projects/ProjectsList";
 import SectionLayout from "../layout/Section/SectionLayout";
+import Profile from "./Profile";
+import Experience from "./Experience";
+import { Education } from "./Education";
 
 const Home = () => {
   return (
     <div>
       <Navbar />
       <Container>
+        <SectionLayout id="Profile">
+          <Profile />
+        </SectionLayout>
+        <SectionLayout title="Experience" id="Experience">
+          <Experience />
+        </SectionLayout>
+        <SectionLayout title="Education" id="Education">
+          <Education />
+        </SectionLayout>
         <SectionLayout title="Projects" id="Projects">
           <ProjectsList />
         </SectionLayout>
